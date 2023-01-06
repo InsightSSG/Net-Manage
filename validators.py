4f6de88e9aeeb8ecb97e98d5c1886a0b8f0c9cfb
Josh Spiers <josh.spiers@insight.com>
2023-01-06 03:35:17 -0500

    Create basic validator that can be used for single-column validation #124

--- validators.py
+++ validators.py
@@ -179,33 +179,39 @@ def f5_vip_availability(db_path, table):
     return df_diff
 
 
-def meraki_device_statuses_availability(db_path, table):
+def validator_single_col(columns,
+                         db_path,
+                         expected,
+                         identifier_col,
+                         table,
+                         validation_col):
     '''
-    Validates the state of Meraki devices based on the 'status' column.
+    A generic validator for collectors that can be validated with the state of
+    a single column--e.g., 'status', 'availability', etc.
 
     Args:
-        db_path (str):  The path to the database
-        table (str):    The name of the table
+        columns (list):         A list of columns to return. One of the columns
+                                must be the one that is being used for
+                                validation.
+        db_path (str):          The path to the database
+        expected (str):         The expected value of 'validation_col' (see
+                                below)
+        identifier_col (str):   The column name to use for identifying a unique
+                                entity (e.g., device, network, organization,
+                                etc)
+        table (str):            The name of the table
+        validation_col (str):   The column name to use for validation
 
     Returns:
         df_diff (obj):  A DataFrame containing any differences
     '''
-    # Get the first and last timestamp for each unique device in the table
-    df_stamps = hp.get_first_last_timestamp(db_path, table, 'mac')
+    return_cols = ',\n'.join(columns)
 
-    # Define the columns to query
-    cols = ['orgId',
-            'networkId',
-            'name',
-            'model',
-            'lanIp',
-            'publicIp',
-            'mac',
-            'status']
-    return_cols = ',\n'.join(cols)
+    # Get the first and last timestamp for each unique device in the table
+    df_stamps = hp.get_first_last_timestamp(db_path, table, identifier_col)
 
     # Create an empty dataframe to store the devices that have changed status
-    df_diff = pd.DataFrame(data=list(), columns=cols)
+    df_diff = pd.DataFrame(data=list(), columns=columns)
 
     # Create the database connection
     con = sl.connect(db_path)
@@ -227,7 +233,7 @@ def meraki_device_statuses_availability(db_path, table):
     # timestamp to the first timestamp. That way we can see exactly what
     # changed--e.g., 'online' to 'alerting', 'dormant' to 'online', etc.
     for idx, row in df_stamps.iterrows():
-        mac = row['mac']
+        unique = row[identifier_col]
         first_ts = row['first_ts']
         last_ts = row['last_ts']
 
@@ -235,16 +241,16 @@ def meraki_device_statuses_availability(db_path, table):
         # to the status in the second timestamp, and stores the results in a
         # dataframe.
         query = f'''select {return_cols} from {table}
-                    where (status = "online"
-                        or status != "online")
+                    where ({validation_col} = "{expected}"
+                        or {validation_col} != "{expected}")
                       and timestamp = "{first_ts}"
-                      and mac = "{mac}"
+                      and {identifier_col} = "{unique}"
                     except
                     select {return_cols} from {table}
-                    where (status = "online"
-                        or status != "online")
+                    where ({validation_col} = "{expected}"
+                        or {validation_col} != "{expected}")
                       and timestamp = "{last_ts}"
-                      and mac = "{mac}"
+                      and {identifier_col} = "{unique}"
                 '''
         df_left = pd.read_sql(query, con)
 
@@ -255,32 +261,67 @@ def meraki_device_statuses_availability(db_path, table):
             # timestamp to the status in the first timestamp, then stores the
             # result in a dataframe.
             query = f'''select {return_cols} from {table}
-                        where (status = "online"
-                            or status != "online")
+                        where ({validation_col} = "{expected}"
+                            or {validation_col} != "{expected}")
                         and timestamp = "{last_ts}"
-                        and mac = "{mac}"
+                        and {identifier_col} = "{unique}"
                         except
                         select {return_cols} from {table}
-                        where (status = "online"
-                            or status != "online")
+                        where ({validation_col} = "{expected}"
+                            or {validation_col} != "{expected}")
                         and timestamp = "{first_ts}"
-                        and mac = "{mac}"
+                        and {identifier_col} = "{unique}"
                     '''
             df_right = pd.read_sql(query, con)
 
             # 'except' will return results if the data in any of the columns
             # changed, so it is necessary to compare the two statuses.
-            original_status = df_left['status'].to_list()
-            new_status = df_right['status'].to_list()
+            original = df_left[validation_col].to_list()
+            new = df_right[validation_col].to_list()
 
-            # If the status changed, then add the new status to 'df_left' as a
-            # new column, then add 'df_left' to 'df_diff'
-            if original_status != new_status:
-                df_left['new_status'] = new_status
+            # If the expected value changed, then add the new value to
+            # 'df_left' as a new column, then add'df_left' to 'df_diff' as a
+            # new row.
+            if original != new:
+                df_left[f'new_{identifier_col}'] = new
                 df_diff = pd.concat([df_diff, df_left])
 
     # Rename the 'status' column to 'original_status'
-    df_diff.rename(columns={'status': 'original_status'},
+    df_diff.rename(columns={identifier_col: f'original_{identifier_col}'},
                    inplace=True)
+
+    return df_diff
+
+
+def meraki_device_statuses_availability(db_path, table):
+    '''
+    Validates the state of Meraki devices based on the 'status' column.
+
+    Args:
+        db_path (str):  The path to the database
+        table (str):    The name of the table
+
+    Returns:
+        df_diff (obj):  A DataFrame containing any differences
+    '''
+    # # Get the first and last timestamp for each unique device in the table
+    # df_stamps = hp.get_first_last_timestamp(db_path, table, 'mac')
+
+    # Define the columns to query
+    cols = ['orgId',
+            'networkId',
+            'name',
+            'model',
+            'lanIp',
+            'publicIp',
+            'mac',
+            'status']
+
+    df_diff = validator_single_col(cols,
+                                   db_path,
+                                   'online',
+                                   'mac',
+                                   table,
+                                   'status')
 
     return df_diff
