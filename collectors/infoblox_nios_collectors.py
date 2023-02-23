614fefa21d6ecff53407de193ae65d6f53fc53b1
Joshua Spiers <josh.spiers@insight.com>
2023-02-23 16:47:59 -0500

    Create collector to get all networks from an Infoblox grid #167

--- collectors/infoblox_nios_collectors.py
+++ collectors/infoblox_nios_collectors.py
@@ -10,6 +10,23 @@ import pandas as pd
 from infoblox_client import connector
 
 
+def create_connector(host, username, password):
+    '''
+    Creates the connector to use to connect to the Infoblox grid.
+
+    Args:
+        host (str):     The host's IP address or FQDN
+        username (str): The user's username
+        password (str): The user's password
+
+    Returns:
+        conn (obj):     The connector
+    '''
+    opts = {'host': host, 'username': username, 'password': password}
+    conn = connector.Connector(opts)
+    return conn
+
+
 def disable_ssl_cert_warning(validate_certs):
     '''
     Disables warnings for SSL cert validation, if the user has passed
@@ -57,8 +74,7 @@ def get_network_containers(host,
     disable_ssl_cert_warning(validate_certs)
 
     # Create the connector to use to connect to the API
-    opts = {'host': host, 'username': username, 'password': password}
-    conn = connector.Connector(opts)
+    conn = create_connector(host, username, password)
 
     # Get the network containers
     response = conn.get_object('networkcontainer', paging=paging)
@@ -78,3 +94,50 @@ def get_network_containers(host,
     df_containers = pd.DataFrame.from_dict(df_data)
 
     return df_containers
+
+
+def get_networks(host,
+                 username,
+                 password,
+                 paging=True,
+                 validate_certs=True):
+    '''
+    Gets all network containers from an Infoblox API.
+
+    Args:
+        host (str):                 The host's IP address or FQDN
+        username (str):             The user's username
+        password (str):             The user's password
+        paging (bool):              Whether to perform paging. Defaults to True
+        validate_certs (bool):      Whether to validate SSL certs
+
+
+    Returns:
+        df_networks (DataFrame):    A DataFrame containing the network
+                                    network containers
+    '''
+    # If the user has passed 'validate_certs=False', then disable SSL cert
+    # warnings.
+    disable_ssl_cert_warning(validate_certs)
+
+    # Create the connector to use to connect to the API
+    conn = create_connector(host, username, password)
+
+    # Get the network containers
+    response = conn.get_object('network', paging=paging)
+
+    # Create the dictionary to store the data needed to create the DataFrame,
+    # then interate over 'response' to create the keys
+    df_data = dict()
+    for item in response:
+        for key, value in item.items():
+            df_data[key] = list()
+
+    # Populate 'df_data'
+    for item in response:
+        for key in df_data:
+            df_data[key].append(item.get(key))
+
+    df_networks = pd.DataFrame.from_dict(df_data)
+
+    return df_networks
