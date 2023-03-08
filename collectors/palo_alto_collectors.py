31eaa550aa36e787e3d1d8123068b37e2fc14fab
Joshua Spiers <josh.spiers@insight.com>
2023-03-08 12:15:04 -0500

    Create collector to get Palo Alto physical interfaces #189

--- collectors/palo_alto_collectors.py
+++ collectors/palo_alto_collectors.py
@@ -90,3 +90,77 @@ def get_arp_table(username,
     df = pd.DataFrame.from_dict(df_data)
 
     return df
+
+
+def get_physical_interfaces(username,
+                            password,
+                            host_group,
+                            play_path,
+                            private_data_dir):
+    """Gets the physical interfaces on Palo Altos.
+
+    Parameters
+    ----------
+    username : str
+        The user's username.
+    password : str
+        The user's password.
+    host_group : str
+        The name of the Ansible inventory host group.
+    nm_path : str
+        The path to the Net-Manage repository.
+    play_path : str
+        The path to the directory containing Ansible playbooks
+    private_data_dir : str
+        The path to the Ansible private data directory
+
+    Returns
+    ----------
+    df : Pandas Dataframe
+        A dataframe containing the physical interfaces.
+
+    Examples
+    ----------
+    >>> df = get_physical_interfaces(username,
+                                     password,
+                                     host_group,
+                                     nm_path,
+                                     play_path,
+                                     private_data_dir)
+    >>> df.columns.to_list()
+    ['device',
+    'name',
+    'duplex',
+    'type',
+    'state',
+    'st',
+    'mac',
+    'mode',
+    'speed',
+    'id']
+    """
+    result = pc.get_physical_interfaces(username,
+                                        password,
+                                        host_group,
+                                        play_path,
+                                        private_data_dir)
+
+    # Create a dictionary to store the data for 'df'.
+    df_data = dict()
+    df_data['device'] = list()
+
+    # Populate 'df_data' from 'result'.
+    for device in result:
+        interfaces = result[device]
+        for item in interfaces:
+            df_data['device'].append(device)
+            for key, value in item.items():
+                if key != 'ae_member':  # Exclude aggregation groups.
+                    if not df_data.get(key):
+                        df_data[key] = list()
+                    df_data[key].append(value)
+
+    # Create the dataframe.
+    df = pd.DataFrame.from_dict(df_data)
+
+    return df
