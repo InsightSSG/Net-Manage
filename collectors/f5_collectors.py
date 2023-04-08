3be0b748d310709cb46270e640a1a01f73170e71
Joshua Spiers <josh.spiers@insight.com>
2023-04-07 21:50:16 -0400

    #215 #216 #217 #219

--- collectors/f5_collectors.py
+++ collectors/f5_collectors.py
@@ -7,6 +7,7 @@ Define F5 collectors
 import ansible_runner
 import ast
 import pandas as pd
+from helpers import helpers as hp
 
 
 def convert_tmsh_output_to_dict(in_data):
@@ -105,6 +106,7 @@ def convert_tmsh_output_to_dict(in_data):
                 if '"' in line:
                     line = line.split('"')
                     line = list(filter(None, line))
+                    line = [_.strip() for _ in line]
                 else:
                     line = line.split()
                 line = [f'"{_}"' for _ in line]
@@ -159,12 +161,86 @@ def convert_tmsh_output_to_dict(in_data):
     return out
 
 
-def f5_get_self_ips(username,
-                    password,
-                    host_group,
-                    play_path,
-                    private_data_dir,
-                    validate_certs=True):
+def get_arp_table(username,
+                  password,
+                  host_group,
+                  nm_path,
+                  play_path,
+                  private_data_dir,
+                  validate_certs=True):
+    """Get the ARP table on F5 LTMs.
+
+    Parameters
+    ----------
+    username : str
+        The username to login to devices.
+    password : str
+        The password to login to the device.
+    host_group : str
+        The Ansible inventory host group.
+    nm_path : str
+        The path to the Net-Manage repository.
+    play_path : str
+        The path to the playbooks directory.
+    private_data_dir : str
+        The path to the Ansible private data directory.
+    validate_certs : bool, optional:
+        Whether to validate SSL certificates.
+
+    Returns
+    ----------
+    df : DataFrame
+        A Pandas Dataframe containing the ARP table.
+    """
+    extravars = {'username': username,
+                 'password': password,
+                 'host_group': host_group,
+                 'command': r'show net arp | grep -v "\\-\\-\\-\\-\|Net::Arp"'}
+
+    if not validate_certs:
+        extravars['validate_certs'] = 'no'
+
+    playbook = f'{play_path}/f5_run_adhoc_command.yml'
+
+    runner = ansible_runner.run(private_data_dir=private_data_dir,
+                                playbook=playbook,
+                                extravars=extravars,
+                                suppress_env_files=True)
+
+    # Create a list to store the ARP data for `df`.
+    df_data = list()
+
+    for event in runner.events:
+        if event['event'] == 'runner_on_ok':
+            event_data = event['event_data']
+            device = event_data['remote_addr']
+
+            output = event_data['res']['stdout_lines'][0]
+
+            # Create the dataframe columns.
+            columns = ['device'] + output[0].split()
+
+            # Parse the output and add it to `df_data`
+            for line in output[1:]:
+                line = [device] + line.split()
+                df_data.append(line)
+
+    # Create `df`.
+    df = pd.DataFrame(df_data, columns=columns).astype(str)
+
+    # Get the MAC OUIs and add them to `df`
+    df_macs = hp.find_mac_vendors(df['HWaddress'], nm_path)
+    df['vendor'] = df_macs['vendor']
+
+    return df
+
+
+def get_self_ips(username,
+                 password,
+                 host_group,
+                 play_path,
+                 private_data_dir,
+                 validate_certs=True):
     """Get the self IPs on F5 LTMs.
 
     Parameters
@@ -190,7 +266,7 @@ def f5_get_self_ips(username,
     extravars = {'username': username,
                  'password': password,
                  'host_group': host_group,
-                 'command': 'list net self'}
+                 'command': 'list net self /*/*'}
 
     if not validate_certs:
         extravars['validate_certs'] = 'no'
@@ -242,7 +318,112 @@ def f5_get_self_ips(username,
 
                     # Add each key in `block` to `df_data`.
                     for key in value:
-                        df_data[key] = list()
+                        if not df_data.get(key):
+                            df_data[key] = list()
+
+                counter += 1
+
+    # Iterate over `data`, adding the values to `df_data`.
+    for key, value in data.items():
+        for item in value:
+            for k in df_data:
+                df_data[k].append(item.get(k))
+
+    # Create `df`.
+    df = pd.DataFrame.from_dict(df_data).astype(str)
+
+    # Make `device` the first column, then return `df`.
+    col_1 = df.pop('device')
+    df.insert(0, 'device', col_1)
+
+    return df
+
+
+def get_vlans(username,
+              password,
+              host_group,
+              play_path,
+              private_data_dir,
+              validate_certs=True):
+    """Gets the VLANs on F5 LTMs.
+
+    Parameters
+    ----------
+    username : str
+        The username to login to devices.
+    password : str
+        The password to login to the device.
+    host_group : str
+        The Ansible inventory host group.
+    play_path : str
+        The path to the playbooks directory.
+    private_data_dir : str
+        The path to the Ansible private data directory.
+    validate_certs : bool, optional:
+        Whether to validate SSL certificates.
+
+    Returns
+    ----------
+    df : DataFrame
+        A Pandas Dataframe containing the VLANs.
+    """
+    extravars = {'username': username,
+                 'password': password,
+                 'host_group': host_group,
+                 'command': 'list net vlan /*/*'}
+
+    if not validate_certs:
+        extravars['validate_certs'] = 'no'
+
+    playbook = f'{play_path}/f5_run_adhoc_command.yml'
+
+    runner = ansible_runner.run(private_data_dir=private_data_dir,
+                                playbook=playbook,
+                                extravars=extravars,
+                                suppress_env_files=True)
+
+    # Create a dictionary to store each self IP.
+    data = dict()
+
+    # Create a dictionary to store the data for `df`
+    df_data = dict()
+
+    for event in runner.events:
+        if event['event'] == 'runner_on_ok':
+            event_data = event['event_data']
+            device = event_data['remote_addr']
+            data[device] = list()
+            output = event_data['res']['stdout_lines'][0]
+
+            # Parse the output and add it to `data``
+            counter = 0
+            for line in output:
+                if line[:8] == 'net vlan':
+                    block = list()
+                    pos = counter
+                    while output[pos][0] != '}':
+                        block.append(output[pos])
+                        pos += 1
+                    block.append('}')
+
+                    # Convert the block to a dictionary then flatten it.
+                    # pprint(block)
+                    block = '\n'.join(block)
+                    block = convert_tmsh_output_to_dict(block)
+
+                    for key, value in block.items():
+                        self_name = key.split()[-1]
+                        value['name'] = self_name
+
+                    # Add the device name to `value`, then add `block` to
+                    # `data`.
+                    value['device'] = device
+                    data[device].append(value)
+
+                    # Add each key in `block` to `df_data`.
+                    for key in value:
+                        if not df_data.get(key):
+                            df_data[key] = list()
 
                 counter += 1
 
