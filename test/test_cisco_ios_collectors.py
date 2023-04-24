ba3d13659b4024c8a335fd19c3b9ee4ae5896d87
Josh Spiers <josh.spiers@insight.com>
2023-04-24 04:45:43 -0400

    nxos and ios get_arp_table collectors fail #225

--- test/test_cisco_ios_collectors.py
+++ test/test_cisco_ios_collectors.py
@@ -27,7 +27,7 @@ def test_get_cam_table(username,
                                           private_data_dir,
                                           interface=None)
 
-    expected = ['device', 'interface', 'mac', 'vlan', 'vendor']
+    expected = ['device', 'Vlan', 'Mac Address', 'Type', 'Ports', 'vendor']
     assert df_cam.columns.to_list() == expected
 
     assert len(df_cam) >= 1
@@ -58,20 +58,20 @@ def main():
     username = os.environ.get('USERNAME')
     password = os.environ.get('PASSWORD')
     host_group_all = os.environ.get('IOS_HOST_GROUP_ALL')
-    # host_group_l2 = os.environ.get('IOS_L2_HOST_GROUP')
+    host_group_l2 = os.environ.get('IOS_L2_HOST_GROUP')
     # host_group_l3 = os.environ.get('IOS_L3_HOST_GROUP')
     # nm_path = os.environ.get('NM_PATH')
     play_path = os.environ.get('PLAY_PATH')
     private_data_dir = os.environ.get('PRIVATE_DATA_DIR')
 
     # Execute tests
-    # test_get_cam_table(username,
-    #                    password,
-    #                    host_group_l2,
-    #                    nm_path,
-    #                    play_path,
-    #                    private_data_dir,
-    #                    interface=None)
+    test_get_cam_table(username,
+                       password,
+                       host_group_l2,
+                       nm_path,
+                       play_path,
+                       private_data_dir,
+                       interface=None)
 
     test_get_interface_descriptions(username,
                                     password,
