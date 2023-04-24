e3342ad98d1e091cd5e0ced5983a42527e45009e
Josh Spiers <josh.spiers@insight.com>
2023-04-23 22:54:30 -0400

    Write unit tests for all collectors #231

--- test/test_cisco_ios_collectors.py
+++ test/test_cisco_ios_collectors.py
@@ -165,7 +165,8 @@ def test_get_interface_descriptions(username,
 def main():
     username = os.environ.get('USERNAME')
     password = os.environ.get('PASSWORD')
-    host_group = os.environ.get('IOS_HOST_GROUP')
+    host_group_l2 = os.environ.get('IOS_L2_HOST_GROUP')
+    host_group_l3 = os.environ.get('IOS_L3_HOST_GROUP')
     nm_path = os.environ.get('NM_PATH')
     play_path = os.environ.get('PLAY_PATH')
     private_data_dir = os.environ.get('PRIVATE_DATA_DIR')
@@ -173,35 +174,18 @@ def main():
     # Execute tests
     test_get_cam_table(username,
                        password,
-                       host_group,
+                       host_group_l2,
                        nm_path,
                        play_path,
                        private_data_dir,
                        interface=None)
 
-    # test_get_all_interfaces(username,
-    #                         password,
-    #                         host_group,
-    #                         nm_path,
-    #                         private_data_dir)
-
-    # test_get_arp_table(username,
-    #                    password,
-    #                    host_group,
-    #                    nm_path,
-    #                    private_data_dir)
-
-    # test_get_logical_interfaces(username,
-    #                             password,
-    #                             host_group,
-    #                             nm_path,
-    #                             private_data_dir)
-
-    # test_get_physical_interfaces(username,
-    #                              password,
-    #                              host_group,
-    #                              nm_path,
-    #                              private_data_dir)
+    test_get_interface_descriptions(username,
+                                    password,
+                                    host_group_l3,
+                                    play_path,
+                                    private_data_dir,
+                                    interface=None)
 
 
 if __name__ == '__main__':
