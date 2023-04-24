549e1a2a4e915b9d4c039c518156d0d0ec6b4a67
Josh Spiers <josh.spiers@insight.com>
2023-04-24 03:33:52 -0400

    Write unit tests for all collectors #231

---
+++ test/test_cisco_nxos_collectors.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+# Change to the Net-Manage repository so imports will work
+nm_path = os.environ.get('NM_PATH')
+os.chdir(f'{nm_path}/test')
+sys.path.append('..')
+from collectors import collectors  # noqa
+
+
+def test_get_cam_table(username,
+                       password,
+                       host_group,
+                       nm_path,
+                       play_path,
+                       private_data_dir,
+                       interface=None):
+    """Test the 'nxos_get_cam_table' collector.
+    """
+    df_cam = collectors.nxos_get_cam_table(username,
+                                           password,
+                                           host_group,
+                                           nm_path,
+                                           play_path,
+                                           private_data_dir,
+                                           interface=None)
+
+    expected = ['device', 'interface', 'mac', 'vlan', 'vendor']
+    assert df_cam.columns.to_list() == expected
+
+    assert len(df_cam) >= 1
+
+
+def test_get_interface_descriptions(username,
+                                    password,
+                                    host_group,
+                                    play_path,
+                                    private_data_dir,
+                                    interface=None):
+    """Test the 'nxos_get_interface_descriptions' collector.
+    """
+    df_desc = collectors.nxos_get_interface_descriptions(username,
+                                                         password,
+                                                         host_group,
+                                                         play_path,
+                                                         private_data_dir,
+                                                         interface=None)
+
+    expected = ['device', 'interface', 'description']
+    assert df_desc.columns.to_list() == expected
+
+    assert len(df_desc) >= 1
+
+
+def main():
+    username = os.environ.get('USERNAME')
+    password = os.environ.get('PASSWORD')
+    host_group_all = os.environ.get('NXOS_HOST_GROUP_ALL')
+    play_path = os.environ.get('PLAY_PATH')
+    private_data_dir = os.environ.get('PRIVATE_DATA_DIR')
+
+    # Execute tests
+    # test_get_cam_table(username,
+    #                    password,
+    #                    host_group_l2,
+    #                    nm_path,
+    #                    play_path,
+    #                    private_data_dir,
+    #                    interface=None)
+
+    test_get_interface_descriptions(username,
+                                    password,
+                                    host_group_all,
+                                    play_path,
+                                    private_data_dir,
+                                    interface=None)
+
+
+if __name__ == '__main__':
+    main()
