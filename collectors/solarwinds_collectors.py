c36c0671158af9f51dc6b3bd51b06dc23f36813f
Joshua Spiers <josh.spiers@insight.com>
2023-06-11 11:45:41 -0400

    Create collector to get group members from Solarwinds NPM #268

--- collectors/solarwinds_collectors.py
+++ collectors/solarwinds_collectors.py
@@ -51,23 +51,29 @@ def get_npm_group_id(server: str,
 def get_npm_group_members(server: str,
                           username: str,
                           password: str,
-                          group_name: str) -> list:
+                          group_name: str = 'all') -> pd.DataFrame:
     """
     Retrieves the names of all the devices within an Orion NPM group.
 
     Args:
-    server (str): The hostname or IP address of the Orion database server.
-    username (str): The username used to authenticate with the Orion API.
-    password (str): The password used to authenticate with the Orion API.
-    group_name (str): The name of the group whose devices should be retrieved.
+    server (str):
+        The hostname or IP address of the Orion database server.
+    username (str):
+        The username used to authenticate with the Orion API.
+    password (str):
+        The password used to authenticate with the Orion API.
+    group_name (str, optional):
+        The name of the group whose devices should be retrieved.
 
     Returns:
-    list: A list of device names.
+    list:
+        A list of device names.
 
     Raises:
-    IndexError: If the specified group name is not found in the Orion NPM
-    database.
-    Exception: If there is an issue connecting to the Orion NPM API.
+    IndexError:
+        If the specified group name is not found in the Orion NPM database.
+    Exception:
+        If there is an issue connecting to the Orion NPM API.
 
     Example usage:
     >>> server = 'myserver.mycompany.com'
@@ -76,18 +82,55 @@ def get_npm_group_members(server: str,
     >>> group_name = 'Switches'
     >>> members = get_npm_group_members(server, username, password, group_name)
     >>> print(members)
-    ['Switch1', 'Switch2', 'Switch3']
+    <class 'pandas.core.frame.DataFrame'>
+    RangeIndex: 23 entries, 0 to 22
+    Data columns (total 1 columns):
+    #   Column   Non-Null Count  Dtype
+    ---  ------   --------------  -----
+    0   devices  23 non-null     object
+    dtypes: object(1)
+    memory usage: 312.0+ bytes
+    None
     """
     swis = SwisClient(server, username, password)
 
-    # Get the group ID for the specified group name
-    g_id = get_npm_group_id(server, username, password, group_name)
+    results = list()
 
-    # Retrieve the devices inside the group
-    results = swis.query(
-        f"SELECT Name FROM Orion.ContainerMembers WHERE ContainerID='{g_id}'"
-    )
-    return [result["Name"] for result in results["results"]]
+    # Retrieve the devices inside the group(s)
+    if group_name != 'all':
+        # Get the group ID for the specified group name
+        g_id = get_npm_group_id(server, username, password, group_name)
+        # Get the group members
+        members = swis.query(
+            f"""SELECT Name FROM Orion.ContainerMembers
+                WHERE ContainerID='{g_id}'"""
+        )
+        members = [result["Name"] for result in members["results"]]
+        for member in members:
+            row = [group_name, g_id, member]
+            results.append(row)
+    else:
+        # Get all of the group names.
+        group_names = get_npm_group_names(server, username, password)
+        group_names = group_names['group_name'].to_list()
+        # Iterate through the groups, adding the members to 'results'.
+        for group in group_names:
+            # Get the group ID for the specified group name
+            g_id = get_npm_group_id(server, username, password, group)
+            members = swis.query(
+                f"""SELECT Name FROM Orion.ContainerMembers
+                    WHERE ContainerID='{g_id}'"""
+            )
+            members = [result["Name"] for result in members["results"]]
+            for member in members:
+                row = [group, g_id, member]
+                results.append(row)
+
+    # Convert to a DataFrame and return the results
+    columns = ['group_name', 'group_id', 'member']
+    df = pd.DataFrame(data=results, columns=columns)
+
+    return df
 
 
 def get_npm_group_names(server: str,
