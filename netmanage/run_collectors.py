d0de3dd19448139ffc5b60d0cfd7e8f320b336b8
Joe <82478941+ngsouse@users.noreply.github.com>
2023-09-07 12:58:15 -0500

    lint

--- netmanage/run_collectors.py
+++ netmanage/run_collectors.py
@@ -265,11 +265,11 @@ def collect(ansible_os: str,
 
         if ansible_os == 'cisco.nxos.nxos':
             result = cnc.nxos_get_bgp_neighbors(nxos_devices_username,
-                                                   nxos_devices_password,
-                                                   hostgroup,
-                                                   netmanage_path,
-                                                   play_path,
-                                                   private_data_dir)
+                                                nxos_devices_password,
+                                                hostgroup,
+                                                netmanage_path,
+                                                play_path,
+                                                private_data_dir)
 
     if collector == 'devices_inventory':
         if ansible_os == 'cisco.dnac':
