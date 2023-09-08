d140e73085463eeac34491707a519e896546b3de
Cory P <cory.peterson@insight.com>
2023-09-07 20:43:35 -0400

    Adding helper to Obscure data for demos.

--- netmanage/helpers/obfuscate_names.py
+++ netmanage/helpers/obfuscate_names.py
@@ -2,17 +2,20 @@
 """
 Database Obfuscation and Reversal Module.
 
-This module provides functionality to obfuscate and reverse data in SQLite databases.
-It uses a pre-defined mapping of columns to apply specific transformations based on the column's semantic role.
-The module uses random character mapping to obfuscate string data.
-This ensures referential integrity while making it visually obscured for demonstrations.
+This module provides functionality to obfuscate and reverse data in SQLite
+databases. It uses a pre-defined mapping of columns to apply specific
+transformations based on the column's semantic role. The module uses random
+character mapping to obfuscate string data. This ensures referential integrity
+while making it visually obscured for demonstrations.
 
 Functions:
     - random_map_transform: Transform a string using a forward mapping.
     - reverse_map_transform: Transform a string using a reverse mapping.
     - consistent_transform: Obfuscate a string based on predefined rules.
-    - copy_table_structure: Copies the schema of a table from one SQLite database to another.
-    - reverse_consistent_transform: Reverse the obfuscation based on predefined rules.
+    - copy_table_structure: Copies the schema of a table from one SQLite
+        database to another.
+    - reverse_consistent_transform: Reverse the obfuscation based on
+        predefined rules.
     - obfuscate_db_data: Obfuscate an SQLite database.
     - reverse_db_data: Reverse the obfuscation on an SQLite database.
 
@@ -27,15 +30,21 @@ import string
 import random
 
 # Define a dictionary that sets rules for renaming columns
-# Keys are column names and values are dictionaries that specify 'ignores' and 'prefix'
+# Keys are column names
+# values are dictionaries that specify 'ignores' and 'prefix'
 columns_to_rename = {
-    'device': {'ignores': ['switch', 'sw', 'rtr', 'router', 'fw', 'firewall', 'local'], 'prefix': 'dev-'},
+    'device': {
+        'ignores': ['switch', 'sw', 'rtr', 'router', 'fw', 'firewall',
+                    'local'],
+        'prefix': 'dev-'},
     'Vlan': {'ignores': ['routed', 'trunk'], 'prefix': 'Vlan-'},
     'vlan': {'ignores': ['routed', 'trunk'], 'prefix': 'vlan-'},
     'nameif': {
-        'ignores': ['management', 'inside', 'outside', 'link', 'dmz', 'pci', 'ext', 'mgt', 'mgmt', 'int', 'vpn'],
+        'ignores': ['management', 'inside', 'outside', 'link', 'dmz', 'pci',
+                    'ext', 'mgt', 'mgmt', 'int', 'vpn'],
         'prefix': 'iface-'},
-    'description': {'ignores': ['sw', 'switch', 'rtr', 'router'], 'prefix': 'desc-'},
+    'description': {
+        'ignores': ['sw', 'switch', 'rtr', 'router'], 'prefix': 'desc-'},
     'notes': {'ignores': ['sw', 'switch', 'rtr', 'router'], 'prefix': 'note-'},
     'desc': {'ignores': ['sw', 'switch', 'rtr', 'router'], 'prefix': 'dsc-'},
     'partition': {'ignores': [], 'prefix': 'part-'},
@@ -45,8 +54,11 @@ columns_to_rename = {
     'pool_name': {'ignores': [], 'prefix': 'pname-'},
     'pool_member': {'ignores': [], 'prefix': 'pmem-'},
     'vip': {'ignores': [], 'prefix': 'vip-'},
-    'peer_group': {'ignores': [], 'prefix': 'AS'},  # Keeping "AS" prefix for bgp
-    'vrf': {'ignores': ['internet', 'dr', 'mgmt', 'management', 'mgt', 'default', 'wan'], 'prefix': 'vrf-'},
+    'peer_group': {'ignores': [], 'prefix': 'AS'},
+    'vrf': {
+        'ignores': ['internet', 'dr', 'mgmt', 'management', 'mgt', 'default',
+                    'wan'],
+        'prefix': 'vrf-'},
     'serial': {'ignores': [], 'prefix': 'srl-'},
     'serialnum': {'ignores': [], 'prefix': 'snum-'},
     'networkId': {'ignores': [], 'prefix': 'netId-'},
@@ -102,8 +114,10 @@ def consistent_transform(s: str, ignores: list = [], prefix: str = '') -> str:
 
     Args:
         s (str): The original string to transform.
-        ignores (list, optional): List of values to ignore while transforming. Defaults to [].
-        prefix (str, optional): Prefix to add to the transformed string. Defaults to ''.
+        ignores (list, optional): List of values to ignore while transforming.
+            Defaults to [].
+        prefix (str, optional): Prefix to add to the transformed string.
+            Defaults to ''.
 
     Returns:
         str: Transformed string.
@@ -118,11 +132,12 @@ def consistent_transform(s: str, ignores: list = [], prefix: str = '') -> str:
             transformed_words.append(word)
         else:
             transformed_words.append(random_map_transform(word))
-    return prefix + ''.join(transformed_words) if prefix else ''.join(transformed_words)
+    return prefix + ''.join(transformed_words) if prefix \
+        else ''.join(transformed_words)
 
 
 def copy_table_structure(source_cursor, dest_cursor, dest_conn, table_name):
-    """Copy the table structure from a source database to a destination database.
+    """Copy the table structure from a source to a destination database
 
     Args:
         source_cursor: SQLite cursor object for the source database.
@@ -138,14 +153,18 @@ def copy_table_structure(source_cursor, dest_cursor, dest_conn, table_name):
     dest_conn.commit()
 
 
-def reverse_consistent_transform(s: str, reverse_map, ignores: list = [], prefix: str = '') -> str:
-    """Reverse the transformation applied to a string back to its original value based on provided rules.
+def reverse_consistent_transform(s: str, reverse_map, ignores: list = [],
+                                 prefix: str = '') -> str:
+    """Reverse the transformation applied to a string back to its original
+    value based on provided rules.
 
     Args:
         s (str): The transformed string.
         reverse_map (dict): The reverse mapping dictionary.
-        ignores (list, optional): List of values to ignore while transforming. Defaults to [].
-        prefix (str, optional): Prefix to remove from the transformed string. Defaults to ''.
+        ignores (list, optional): List of values to ignore while transforming.
+            Defaults to [].
+        prefix (str, optional): Prefix to remove from the transformed string.
+            Defaults to ''.
 
     Returns:
         str: Original string.
@@ -171,7 +190,8 @@ def reverse_consistent_transform(s: str, reverse_map, ignores: list = [], prefix
 
 
 def obfuscate_db_data(source_db_path: str, dest_db_path: str):
-    """Obfuscate the database data from a source database to a destination database.
+    """Obfuscate the database data from a source database to a destination
+    database.
 
     Args:
         source_db_path (str): Path to the source SQLite database.
@@ -184,9 +204,12 @@ def obfuscate_db_data(source_db_path: str, dest_db_path: str):
     dest_cursor = dest_conn.cursor()
 
     # Create and populate the mapping table
-    dest_cursor.execute("CREATE TABLE IF NOT EXISTS char_map (original TEXT, mapped TEXT);")
+    dest_cursor.execute(
+        "CREATE TABLE IF NOT EXISTS char_map (original TEXT, mapped TEXT);")
     for original, mapped in FORWARD_MAP.items():
-        dest_cursor.execute("INSERT INTO char_map (original, mapped) VALUES (?, ?);", (original, mapped))
+        dest_cursor.execute(
+            "INSERT INTO char_map (original, mapped) VALUES (?, ?);",
+            (original, mapped))
     dest_conn.commit()
 
     source_cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -216,14 +239,16 @@ def obfuscate_db_data(source_db_path: str, dest_db_path: str):
                     continue
                 rules = columns_to_rename.get(col, {})
                 if col in columns_to_rename:
-                    new_value = consistent_transform(original_value, rules.get("ignores", []),
-                                                     prefix=rules.get("prefix", ""))
+                    new_value = consistent_transform(original_value, rules.get(
+                        "ignores", []), prefix=rules.get("prefix", ""))
                 else:
                     new_value = original_value
                 new_values.append(new_value)
 
             placeholders = ", ".join(["?" for _ in new_values])
-            dest_cursor.execute(f"INSERT INTO {table_name} VALUES ({placeholders})", tuple(new_values))
+            dest_cursor.execute(
+                f"INSERT INTO {table_name} VALUES ({placeholders})",
+                tuple(new_values))
 
         dest_conn.commit()
 
@@ -232,7 +257,8 @@ def obfuscate_db_data(source_db_path: str, dest_db_path: str):
 
 
 def reverse_db_data(source_db_path: str, dest_db_path: str):
-    """Reverse the obfuscation of database data from a source to a destination database.
+    """Reverse the obfuscation of database data from a source to a destination
+    database.
 
     Args:
         source_db_path (str): Path to the source SQLite database.
@@ -248,7 +274,8 @@ def reverse_db_data(source_db_path: str, dest_db_path: str):
     char_map_data = source_cursor.fetchall()
     reverse_map = {mapped: original for original, mapped in char_map_data}
 
-    source_cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    source_cursor.execute(
+        "SELECT name FROM sqlite_master WHERE type='table';")
     tables = source_cursor.fetchall()
 
     for table in tables:
@@ -275,14 +302,21 @@ def reverse_db_data(source_db_path: str, dest_db_path: str):
                     continue
                 rules = columns_to_rename.get(col, {})
                 if col in columns_to_rename:
-                    new_value = reverse_consistent_transform(original_value, reverse_map, rules.get("ignores", []),
-                                                             prefix=rules.get("prefix", ""))
+                    new_value = reverse_consistent_transform(original_value,
+                                                             reverse_map,
+                                                             rules.get(
+                                                                 "ignores",
+                                                                 []),
+                                                             prefix=rules.get(
+                                                                 "prefix", ""))
                 else:
                     new_value = original_value
                 new_values.append(new_value)
 
             placeholders = ", ".join(["?" for _ in new_values])
-            dest_cursor.execute(f"INSERT INTO {table_name} VALUES ({placeholders})", tuple(new_values))
+            dest_cursor.execute(
+                f"INSERT INTO {table_name} VALUES ({placeholders})",
+                tuple(new_values))
 
         dest_conn.commit()
 
@@ -293,7 +327,9 @@ def reverse_db_data(source_db_path: str, dest_db_path: str):
 # Test functions
 def test_random_map_transform_and_reverse():
     test_str = "msp-core-switch-01"
-    print(f"Testing random_map_transform and reverse_map_transform with input: '{test_str}'")
+    print(
+        f"Testing random_map_transform and reverse_map_transform with input: "
+        f"'{test_str}'")
 
     transformed_str = random_map_transform(test_str)
     print(f"Transformed string: '{transformed_str}'")
@@ -301,21 +337,26 @@ def test_random_map_transform_and_reverse():
     reversed_str = reverse_map_transform(transformed_str, REVERSE_MAP)
     print(f"Reversed string: '{reversed_str}'")
 
-    assert reversed_str == test_str, "Test for random_map_transform and reverse_map_transform failed."
+    assert reversed_str == test_str, \
+        "Test for random_map_transform and reverse_map_transform failed."
 
 
 def test_consistent_transform_and_reverse():
     test_str = "msp-core-switch-01"
     rules = {'ignores': ['switch'], 'prefix': 'test-'}
-    print(f"Testing consistent_transform and reverse_consistent_transform with input: '{test_str}'")
+    print(
+        f"Testing consistent_transform and "
+        f"reverse_consistent_transform with input: '{test_str}'")
 
     transformed_str = consistent_transform(test_str, **rules)
     print(f"Transformed string: '{transformed_str}'")
 
-    reversed_str = reverse_consistent_transform(transformed_str, REVERSE_MAP, **rules)
+    reversed_str = reverse_consistent_transform(transformed_str, REVERSE_MAP,
+                                                **rules)
     print(f"Reversed string: '{reversed_str}'")
 
-    assert reversed_str == test_str, "Test for consistent_transform and reverse_consistent_transform failed."
+    assert reversed_str == test_str, ("Test for consistent_transform and "
+                                      "reverse_consistent_transform failed.")
 
 
 # Main test function
