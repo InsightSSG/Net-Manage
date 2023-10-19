7ad68c322fbe25b930429ece86e6a7be18bf8cb4
Josh Spiers <josh.spiers@insight.com>
2023-10-19 12:47:41 -0400

    IOS inventory collector fails on virtual devices #481

--- netmanage/parsers/cisco_ios_parsers.py
+++ netmanage/parsers/cisco_ios_parsers.py
@@ -756,54 +756,49 @@ def ios_parse_inventory(runner: dict) -> pd.DataFrame:
         raise ValueError("The input is None or empty")
 
     # Create a dictionary to store the inventory data.
-    columns = ['device',
-               'name',
-               'description',
-               'pid',
-               'vid',
-               'serial']
-    df_data = dict()
-    for col in columns:
-        df_data[col] = list()
+    columns = ['device', 'name', 'description', 'pid', 'vid', 'serial']
+    df_data = {col: [] for col in columns}
 
     # Parse the output and add it to 'df_data'
     for event in runner.events:
         if event["event"] == "runner_on_ok":
             event_data = event["event_data"]
-
             device = event_data["remote_addr"]
-
-            data = list(filter(
-                None, event_data["res"]["stdout"][0].split("\n")))
+            data = list(
+                filter(None, event_data["res"]["stdout"][0].split("\n")))
 
             if data:
-                # Iterate through data (each hardware entry has 3 lines).
-                for i in range(0, len(data), 3):
-                    df_data['device'].append(device)
-                    # Split the first line to extract name and description
-                    name_desc = data[i].split(", DESCR: ")
-                    df_data['name'].append(
-                        name_desc[0].replace('NAME: "', '').
-                        replace('"', '').strip())
-                    df_data['description'].append(
-                        name_desc[1].replace('"', '').strip())
+                for i in range(0, len(data)):
+                    # Handling the "NAME: ... , DESCR: ..." lines
+                    if data[i].startswith("NAME: "):
+                        try:
+                            df_data['device'].append(device)
+                            # Split the first line to extract name, description
+                            name_desc = data[i].split(", DESCR: ")
+                            df_data['name'].append(
+                                name_desc[0].replace('NAME: "', '').replace(
+                                    '"', '').strip())
+                            df_data['description'].append(
+                                name_desc[1].replace('"', '').strip())
 
-                    # Split the second line to extract PID, VID and SN
-                    pid_vid_sn = data[i+1].split(", ")
-                    df_data['pid'].append(
-                        pid_vid_sn[0].replace('PID: ', '').strip())
-                    df_data['vid'].append(
-                        pid_vid_sn[1].replace('VID: ', '').strip())
+                            # Next line will have the PID, VID, SN data
+                            pid_vid_sn = data[i+1].split(", ")
+                            df_data['pid'].append(
+                                pid_vid_sn[0].replace('PID: ', '').strip())
+                            df_data['vid'].append(
+                                pid_vid_sn[1].replace('VID: ', '').strip())
+                            sn_value = pid_vid_sn[2].replace(
+                                'SN: ', '').strip()
+                            if sn_value == "SN:":
+                                sn_value = ""
+                            df_data['serial'].append(sn_value)
 
-                    # If SN value is "SN:", replace with an empty string.
-                    sn_value = pid_vid_sn[2].replace('SN: ', '').strip()
-                    if sn_value == "SN:":
-                        sn_value = ""
-                    df_data['serial'].append(sn_value)
+                        except (IndexError, ValueError):
+                            print(f"Error processing: {data[i]}")
+                            continue
 
     # Create the DataFrame and return it.
     df = pd.DataFrame(df_data)
-
     return df
 
 
